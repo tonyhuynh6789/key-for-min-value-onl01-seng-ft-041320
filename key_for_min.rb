ec5b9d112a2885f1fe84fb235ca7f010ccc15e8b
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-24 17:20:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,16 +17,3 @@ def key_for_min_value(name_hash)
 end
 
 
-def key_for_min_value(name_hash)
-  smallest_keys = nil
-  smallest_values = nil
-  
-  name_hash.each do |keys, values|
-  if smallest_values == nil || values < smallest_values
-    smallest_values = values
-    smallest_keys = keys
-  end   
-  end
-smallest_keys
-end 
-© 2020 GitHub, In
